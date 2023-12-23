b9cdcceb61b0b7ead14dcbd23e0eee5eaf36eea8
moinkhan07 <moin6k@gmail.com>
2023-12-23 12:08:15 +0530

    User register and login page frontend done

--- Level 2/Job Board/Frontend/job-board-application/src/Components/LogIn.js
+++ Level 2/Job Board/Frontend/job-board-application/src/Components/LogIn.js
@@ -1,20 +1,25 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom';
+import {Link,NavLink} from 'react-router-dom';
 
 const LogIn = () => {
+  const navStyleLink = ()=>{
+    return{
+      marginTop: "20px",
+      textAlign: "center",
+      cursor: "pointer",
+      textDecoration: "none",
+      color:"black"
+    }
+  }
   return (
-    // <div id="loginDiv">
-    // <Link to={'/'}><img src={require('../Assets/logo.jpg')} /></Link>
-    // <h2>Welcome To Movie Venture</h2>
-    // <input value={credentials.userEmail} onChange={handleChange} name="userEmail" placeholder="Enter Email" type={"email"}/>
-    // <input value={credentials.userPassword} onChange={handleChange} name="userPassword"  placeholder="Enter Password" type={"password"}/>
-    // <button onClick={loginUser}>Log In</button>
-    // <hr/>
-    // <NavLink to={'/signup'} style={navStyleLink}><p id="newUser">New User? Sign Up</p></NavLink>
-    // </div>
-    <div>
-      <h1>LogIn</h1>
-     <NavLink to={'/register'}><p>Register</p></NavLink>
+    <div id="loginDiv">
+    <Link to={'/'}><img src={require('../Assets/logo.png')} /></Link>
+    <h2>Welcome To Job Board</h2>
+    <input  name="userEmail" placeholder="Enter Email" type={"email"}/>
+    <input  name="userPassword"  placeholder="Enter Password" type={"password"}/>
+    <button>Log In</button>
+    <hr/>
+    <NavLink to={'/register'} style={navStyleLink}><p id="newUser">New User? Sign Up</p></NavLink>
     </div>
 
   )
--- Level 2/Job Board/Frontend/job-board-application/src/Components/Register.js
+++ Level 2/Job Board/Frontend/job-board-application/src/Components/Register.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import {Link,NavLink} from 'react-router-dom';
 
 const Register = () => {
+  const navStyleLink = () => {
+    return {
+      marginTop: "20px",
+      textAlign: "center",
+      cursor: "pointer",
+      textDecoration: "none",
+      color:"black"
+    };
+  };
   return (
-    <div>Register</div>
+    <>
+      <div id="signupDiv">
+        <Link to={'/'}><img src={require('../Assets/logo.png')} /></Link>
+        <h2>Welcome To Job Board</h2>
+        <input  name="firstName"  placeholder="Enter FirstName" type={"text"}/>
+        <input  name="lastName"  placeholder="Enter LastName" type={"text"}/>
+        <input  name="userEmail" placeholder="Enter Email" type={"email"}/>
+        <input  name="userPassword" placeholder="Enter Password" type={"password"} />
+        <button>Register</button>
+        <hr/>
+        <NavLink to={'/login'} style={navStyleLink}><p id="already">Already have an account? Log In</p></NavLink>
+      </div>
+    </>
   )
 }
 

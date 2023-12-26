521ce1c150f120ab763e9db8d2c976b8ac83d20b
moinkhan07 <moin6k@gmail.com>
2023-12-26 19:23:45 +0530

    employer and user profile setup done

--- Level 2/Job Board/Frontend/job-board-application/src/App.js
+++ Level 2/Job Board/Frontend/job-board-application/src/App.js
@@ -6,14 +6,18 @@ import './Styles/LogIn.css';
 import './Styles/Register.css';
 import './Styles/PostJob.css';
 import './Styles/JobDetails.css';
+import './Styles/UserDashboard.css';
+import './Styles/EmployerDashboard.css';
 import JobList from './Components/JobList';
 import JobSearch from './Components/JobSearch.js';
 import Welcome from './Components/Welcome.js';
 import Navbar from './Components/Navbar.js';
 import LogIn from './Components/LogIn.js';
 import Register from './Components/Register.js';
-import PostJob from './Components/PostJob';
-import JobDetails from './Components/JobDetails'
+import PostJob from './Components/PostJob.js';
+import JobDetails from './Components/JobDetails.js'
+import UserDashboard from './Components/UserDashboard.js'
+import EmployerDashboard from './Components/EmployerDashboard'
 import {Routes,Route} from 'react-router-dom'
 
 function App() {
@@ -25,6 +29,8 @@ function App() {
       <Route path='/register' element={ [ <Navbar />, <Register /> ]} />
       <Route path='/postjob' element={ [ <Navbar />, <PostJob /> ]} />
       <Route path='/detailjob' element={ [ <Navbar />, <JobDetails /> ]} />
+      <Route path='/userdashboard' element={ [ <Navbar />, <UserDashboard /> ]} />
+      <Route path='/employerdashboard' element={ [ <Navbar />, <EmployerDashboard /> ]} />
      </Routes>
     </div>
   );
--- Level 2/Job Board/Frontend/job-board-application/src/Components/Navbar.js
+++ Level 2/Job Board/Frontend/job-board-application/src/Components/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React,{ useState } from 'react';
 import logo from '../Assets/logo.png';
-import {Link,NavLink,useNavigate} from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [dropdownVisible, setDropdownVisible] = useState(false);
   const navStyleLink = ()=>{
     return{
       cursor: "pointer",
@@ -22,6 +23,26 @@ const Navbar = () => {
     } else {
       window.alert("Please login as employer to post a job.");
     }
+  };
+  const handleEmailClick = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleProfileClick = () => {
+    const userEmail = localStorage.getItem('userEmail');
+    const employerEmail = localStorage.getItem('employerEmail');
+
+    if (userEmail) {
+      navigate('/userdashboard');
+    } else if (employerEmail) {
+      navigate('/employerdashboard');
+    }
+  };
+
+  const handleLogoutClick = () => {
+    localStorage.removeItem('userEmail');
+    localStorage.removeItem('employerEmail');
+    setDropdownVisible(false);
   };
   return (
     <div id='navbar'>
@@ -42,7 +63,43 @@ const Navbar = () => {
         <p onClick={pendingAlert}>Contact</p>
       </div>
       <div className="right">
-        <NavLink to={'/login'} style={navStyleLink}><p>Log in</p></NavLink>
+        {/* <NavLink to={'/login'} style={navStyleLink}><p>Log in</p></NavLink> */}
+        {localStorage.getItem('userEmail') || localStorage.getItem('employerEmail') ? (
+          <>
+            <div id='afterLoggedIn'
+              style={{ position: 'relative', cursor: 'pointer' }}
+              onClick={handleEmailClick}
+            >
+              <p style={{ border: '1px solid #ccc',fontSize:'17px',fontWeight:'600' ,padding: '10px 12px', borderRadius: '4px' }} >
+                {localStorage.getItem('userEmail') || localStorage.getItem('employerEmail')}
+              </p>
+              {dropdownVisible && (
+                <div s style={{
+                  width:'105px',
+                  position: 'absolute',
+                  top: '100%',
+                  right: 14,
+                  zIndex: 1,
+                  textAlign:'center',
+                  border: '1px solid #ccc',
+                  borderRadius: '4px',
+                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                }}>
+                  <p style={{ fontSize:'17px',width:'100%',height:'35px',display:'flex',alignItems:'center',justifyContent:'center',borderBottom:'1px solid white' }}  onClick={handleProfileClick}>
+                    Profile
+                  </p>
+                  <p style={{color:'red', fontSize:'17px',width:'100%',height:'35px',display:'flex',alignItems:'center',justifyContent:'center' }}  onClick={handleLogoutClick}>
+                    Logout
+                  </p>
+                </div>
+              )}
+            </div>
+          </>
+        ) : (
+          <NavLink to={'/login'} style={navStyleLink}>
+            <p>Log in</p>
+          </NavLink>
+        )}
         <button onClick={handlePostJobClick}>Post A Job</button>
       </div>
     </div>
